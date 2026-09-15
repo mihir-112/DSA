27eb1b7c2d75299e415d3c5ee3873d4a0cbd9317
mihir <2025cs_mihiresahoo_d@nie.ac.in>
2026-09-15 12:45:16 +0530

    Update program9.c

--- DSA/program9.c
+++ DSA/program9.c
@@ -1,151 +1,143 @@
-#include <stdio.h>
+#include<stdio.h>
 
-char stack[100];
-int top = -1;
+char stack[50];
+int top=-1;
 
 void push(char ch)
 {
-    top++;
-    stack[top] = ch;
+stack[++top]=ch;
 }
 
 char pop()
 {
-    char ch;
-
-    ch = stack[top];
-    top--;
-
-    return ch;
+return stack[top--];
 }
 
-int priority(char ch)
+int precedence(char ch)
 {
-    if (ch == '^')
-        return 3;
-
-    if (ch == '*' || ch == '/')
-        return 2;
-
-    if (ch == '+' || ch == '-')
-        return 1;
-
-    return 0;
+if(ch=='^')
+return 3;
+if(ch=='*'||ch=='/')
+return 2;
+if(ch=='+'||ch=='-')
+return 1;
+return -1;
 }
 
-int main()
+int isOperator(char ch)
 {
-    char infix[100];
-    char postfix[100];
-
-    int i = 0;
-    int j = 0;
-
-    printf("Enter infix expression: ");
-    scanf("%s", infix);
-
-    while (infix[i] != '\0')
-    {
-        char ch = infix[i];
-
-        if ((ch >= 'a' && ch <= 'z') ||
-            (ch >= 'A' && ch <= 'Z') ||
-            (ch >= '0' && ch <= '9'))
-        {
-            postfix[j] = ch;
-            j++;
-        }
-
-        else if (ch == '(')
-        {
-            push(ch);
-        }
-
-        else if (ch == ')')
-        {
-            while (top != -1 && stack[top] != '(')
-            {
-                postfix[j] = pop();
-                j++;
-            }
-
-            pop();
-        }
-
-        else
-        {
-            while (top != -1 &&
-                   priority(stack[top]) >= priority(ch))
-            {
-                postfix[j] = pop();
-                j++;
-            }
-
-            push(ch);
-        }
-
-        i++;
-    }
-
-    while (top != -1)
-    {
-        postfix[j] = pop();
-        j++;
-    }
-
-    postfix[j] = '\0';
-
-    printf("Postfix expression = %s", postfix);
-
-    return 0;
-}
-
-
-// postfix evaluation
-/* #include <stdio.h>
-int stack[100];
-int top=-1;
-void push(int value)
-{
-top++;
-stack[top]=value;
-}
-int pop()
-{
-int value;
-value=stack[top];
-top--;
-return value;
-}
-int main()
-{
-char postfix[100];
-int i=0;
-printf("Enter postfix expression: ");
-scanf("%s",postfix);
-while(postfix[i]!='\0')
-{
-char ch=postfix[i];
-if(ch>='0'&&ch<='9')
-{
-push(ch-'0');
-}
-else
-{
-int b=pop();
-int a=pop();
-if(ch=='+')
-push(a+b);
-else if(ch=='-')
-push(a-b);
-else if(ch=='*')
-push(a*b);
-else if(ch=='/')
-push(a/b);
-}
-i++;
-}
-printf("Result=%d",pop());
+if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^')
+return 1;
 return 0;
 }
-*/
+
+void infixtopostfix(char in[50])
+{
+char postfix[50];
+int i,j=0;
+
+for(i=0;in[i]!='\0';i++)
+{
+if((in[i]>='A'&&in[i]<='Z')||(in[i]>='a'&&in[i]<='z')||(in[i]>='0'&&in[i]<='9'))
+postfix[j++]=in[i];
+
+else if(in[i]=='(')
+push(in[i]);
+
+else if(in[i]==')')
+{
+while(top!=-1&&stack[top]!='(')
+postfix[j++]=pop();
+pop();
+}
+
+else if(isOperator(in[i]))
+{
+while(top!=-1&&stack[top]!='('&&precedence(stack[top])>=precedence(in[i]))
+postfix[j++]=pop();
+
+push(in[i]);
+}
+}
+
+while(top!=-1)
+postfix[j++]=pop();
+
+postfix[j]='\0';
+
+printf("Postfix expression=%s",postfix);
+}
+
+int main()
+{
+char infix[50];
+
+printf("Enter the expression:");
+scanf("%s",infix);
+
+infixtopostfix(infix);
+
+return 0;
+}
+
+
+
+#include<stdio.h>
+
+int stack[50];
+int top=-1;
+
+void push(int value)
+{
+stack[++top]=value;
+}
+
+int pop()
+{
+return stack[top--];
+}
+
+int main()
+{
+char postfix[50];
+int i;
+int a,b;
+
+printf("Enter postfix expression:");
+scanf("%s",postfix);
+
+for(i=0;postfix[i]!='\0';i++)
+{
+if(postfix[i]>='0'&&postfix[i]<='9')
+push(postfix[i]-'0');
+
+else
+{
+b=pop();
+a=pop();
+
+if(postfix[i]=='+')
+push(a+b);
+else if(postfix[i]=='-')
+push(a-b);
+else if(postfix[i]=='*')
+push(a*b);
+else if(postfix[i]=='/')
+push(a/b);
+else if(postfix[i]=='^')
+{
+int result=1;
+int j;
+
+for(j=0;j<b;j++)
+result=result*a;
+
+push(result);
+}
+}
+}
+
+printf("Result=%d",pop());
+
+return 0;
+}
